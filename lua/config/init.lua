f9c1d2a1da1083fe4a66463319de5e0b85312139
monstrasitix <injectedinferno@gmail.com>
2024-06-09 20:31:38 +0100

    Mason config

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,32 +1,32 @@
 require "config.remap"
 require "config.set"
-require "config.lazy"
+require "config.lazier"
 
 require("lazy").setup {
     {
         "olimorris/onedarkpro.nvim",
         priority = 1000,
     },
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		cmd = "TSUpdate",
-	},
-	{
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{"mbbill/undotree"},
-	{"tpope/vim-fugitive"},
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-	},
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        cmd = "TSUpdate",
+    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {"mbbill/undotree"},
+    {"tpope/vim-fugitive"},
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+    },
     {
         "startup-nvim/startup.nvim",
         dependencies = {
@@ -34,15 +34,16 @@ require("lazy").setup {
             "nvim-telescope/telescope.nvim",
         },
     },
-	{"williamboman/mason.nvim"},
-	{"williamboman/mason-lspconfig.nvim"},
-	{"neovim/nvim-lspconfig"},
-	{"hrsh7th/nvim-cmp"},
-	{"hrsh7th/cmp-nvim-lsp"},
-	{"hrsh7th/cmp-nvim-lua"},
-	{"hrsh7th/cmp-buffer"},
-	{"hrsh7th/cmp-path"},
-	{"L3MON4D3/LuaSnip"},
-	{"saadparwaiz1/cmp_luasnip"},
-	{"rafamadriz/friendly-snippets"},
+    {"williamboman/mason.nvim"},
+    {"williamboman/mason-lspconfig.nvim"},
+    {"neovim/nvim-lspconfig"},
+    {"hrsh7th/nvim-cmp"},
+    {"hrsh7th/cmp-nvim-lsp"},
+    {"hrsh7th/cmp-nvim-lua"},
+    {"hrsh7th/cmp-buffer"},
+    {"hrsh7th/cmp-path"},
+    {"L3MON4D3/LuaSnip"},
+    {"saadparwaiz1/cmp_luasnip"},
+    {"rafamadriz/friendly-snippets"},
+    {"WhoIsSethDaniel/mason-tool-installer.nvim"},
 }
